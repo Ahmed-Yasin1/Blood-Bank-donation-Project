2e4cf35a688019f3539833848db0765995d9b3f8
AhmedYasin <xasancumar063@gmail.com>
2026-08-03 14:51:38 +0300

    Create emergency module models and controllers

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,13 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import connectDB from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
+import emergencyRoutes from './routes/emergencyRoutes.js'
+import notificationRoutes from './routes/notificationRoutes.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-dotenv.config()
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
 await connectDB()
 
 const app = express()
@@ -17,6 +19,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRoutes)
+app.use('/api/emergency', emergencyRoutes)
+app.use('/api/notification', notificationRoutes)
 
 
 
